1a3c76fc3e8e2224c8185fe5bc4800f00dfc7f77
minijuan124 <minijuan124@gmail.com>
2021-06-23 15:51:50 -0400

    card generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,27 @@
 /* eslint-disable */
-import "bootstrap";
 import "./style.css";
+let randomSuit = () => {
+  let suit = ["♦", "♥", "♠", "♣"];
+  let suitAtRandom = suit[Math.round(Math.random() * (suit.length - 1))];
+  return suitAtRandom;
+};
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let randomNumber = () => {
+  let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "K", "Q", "A"];
+  let numbersAtRadnom =
+    numbers[Math.round(Math.random() * (numbers.length - 1))];
+  return numbersAtRadnom;
+};
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let number = document.querySelector(".number");
+  number.textContent = randomNumber();
+
+  let suit = randomSuit();
+
+  let topSuit = document.querySelector(".top-suit");
+  topSuit.textContent = suit;
+
+  let bottomSuit = document.querySelector(".bottom-suit");
+  bottomSuit.textContent = suit;
 };
